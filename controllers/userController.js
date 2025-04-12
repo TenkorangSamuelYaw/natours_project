a209ab9f63648ad21b348eaa449ca0c00c06531a
Tenkorang Samuel Yaw <stuga718@gmail.com>
2025-04-12 14:16:28 +0000

    Add current user data update functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import AppError from '../utils/appError.js';
 import {User} from './../models/userModel.js';
 import catchAsyncError from './../utils/catchAsync.js';
 
@@ -11,6 +12,28 @@ export const getAllUsers = catchAsyncError(async(req, res, next) => {
       users: users
     }
   });
+});
+// NOTE User data updated here, user password updated in the authController
+export const updateMe = catchAsyncError(async (req, res, next) => {
+  // 1. Check and create error if user tried to update password
+  if (req.body.password || req.body.confirmPassword) {
+    next(new AppError('This route is not for paasword update. Please use /updateMyPassword', 400));
+  }
+
+  // 2. If not update user document
+  // NOTE Can't use user.save() here because some fields like password are required and we're not updating those fields
+  const {name, email} = req.body;
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { name, email },
+    { new: true, runValidators: true },
+  );
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  })
 });
 
 export const getUser = (req, res) => {
