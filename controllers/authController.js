f767114659466b8709a4b4fb9367380f75431dd7
Tenkorang Samuel Yaw <stuga718@gmail.com>
2025-04-14 04:41:47 +0000

    Send JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,14 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const { password, ...userData } = user.toObject(); // Exclude the password from the response
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json({
     status: 'success',
     token: token,
