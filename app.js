97fa9f43aa9e7f47acd9df9b88ac499cd510f534
Tenkorang Samuel Yaw <stuga718@gmail.com>
2025-04-15 20:54:33 +0000

    Add rate limiting to IP

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
+import {rateLimit} from 'express-rate-limit';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +19,14 @@ app.use(express.json()); // Middleware that parses data from the client to the s
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // This middleware gives a log of the incoming request in the console
 }
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Allow 100 requests from same IP per which window(15 mins)
+  message: "Too many requests from this IP, please try again in 15 minutes!"
+});
+app.use('/api', limiter); // Apply to all routes another /api
+
 app.use(express.static(`${__dirname}/public`));
 // Creating my own middleware here
 app.use((req, res, next) => {
