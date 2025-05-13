50514dd5df680bca4bd7111b382d97842f481771
Tenkorang Samuel Yaw <stuga718@gmail.com>
2025-05-13 02:54:06 +0000

    Feature(Auth): Add missing auth checks and refine role based access

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,24 +21,29 @@ import {
 
 const router = express.Router();
 
+// You don't need to be logged in to perform any of the actions below
 router.post('/signup', signUp);
 router.post('/login', login);
-
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
-router.patch('/updateMyPassword', protect, updatePassword);
 
-router.get('/me', protect, getMe, getUser);
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe); // When the user deletes his account, it is removed but made inactive
 
+// You need to be logged in to perform any of the actions below
+router.use(protect);
+router.patch('/updateMyPassword', updatePassword);
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe); // When the user deletes his account, it is not removed but made inactive
+
+// Only admins can perform the actions below
+router.use(restrictTo('admin'))
 router.route('/').get(getAllUsers).post(createUser); // In future see how when someone is on this endpoint, the person will be redirected to /signUp
 
 router
   .route('/:id')
   .get(getUser)
-  .patch(protect, updateUser)
-  .delete(protect, restrictTo('admin'), deleteUser); // Only admins can permanently remove users
+  .patch(updateUser)
+  .delete(restrictTo('admin'), deleteUser); // Only admins can permanently remove users
 
 export default router; // ES 6 approach
 // module.exports = router; Common JS approach
