2f7f1f7ca86781e25e124f7a9866136d8a29db19
Tenkorang Samuel Yaw <stuga718@gmail.com>
2025-05-17 14:39:44 +0000

    perf(model): add indexes to improve database read performance

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -76,7 +76,7 @@ export const getAll = (Model) =>
       .sort()
       .limitfields()
       .paginate();
-    const doc = await features.query.explain();
+    const doc = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
